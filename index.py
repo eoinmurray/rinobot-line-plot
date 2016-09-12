78ff5e3305ed4f4e4f168d9383d7a954d3557cb6
eoinmurray <amonecho1@gmail.com>
2016-09-12 18:04:51 +0100

    added optional args

--- index.py
+++ index.py
@@ -1,20 +1,30 @@
 import sys
 import os
+import argparse
 import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
 
-output_format = '.png'
-
 matplotlib.rcParams['savefig.dpi'] = 2 * matplotlib.rcParams['savefig.dpi']
 
-def main(filepath):
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filepath', type=str)
+    parser.add_argument('--xmin', type=float)
+    parser.add_argument('--xmax', type=float)
+    parser.add_argument('--ymin', type=float)
+    parser.add_argument('--ymax', type=float)
+
+    args = parser.parse_args()
+    filepath = args.filepath
+
     filename_without_ext = os.path.splitext(filepath)[0]
     data = np.loadtxt(filepath)
     x = data[:, 0]
     y = data[:, 1:]
     plt.plot(x, y)
-    plt.savefig(filename_without_ext + output_format)
 
-if __name__ == "__main__":
-    main(sys.argv[1])
+    plt.xlim([args.xmin, args.xmax])
+    plt.ylim([args.ymin, args.ymax])
+
+    plt.savefig(filename_without_ext + '-line-plot.png')
