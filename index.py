661ab231871c2fb8c3e808c52da127019dfd1fc9
eoinmurray <amonecho1@gmail.com>
2016-09-18 11:24:17 +0100

    started using rinobot_plugin to organise args and output

--- index.py
+++ index.py
@@ -1,30 +1,29 @@
-import sys
-import os
-import argparse
+import rinobot_plugin as bot
 import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
 
 matplotlib.rcParams['savefig.dpi'] = 2 * matplotlib.rcParams['savefig.dpi']
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument('filepath', type=str)
-    parser.add_argument('--xmin', type=float)
-    parser.add_argument('--xmax', type=float)
-    parser.add_argument('--ymin', type=float)
-    parser.add_argument('--ymax', type=float)
-
-    args = parser.parse_args()
-    filepath = args.filepath
-
-    filename_without_ext = os.path.splitext(filepath)[0]
-    data = np.loadtxt(filepath)
+def main():
+    filepath = bot.filepath()
+    data = bot.loadfile(filepath)
     x = data[:, 0]
     y = data[:, 1:]
     plt.plot(x, y)
 
-    plt.xlim([args.xmin, args.xmax])
-    plt.ylim([args.ymin, args.ymax])
+    xmin = bot.get_arg('xmin', type=float)
+    xmax = bot.get_arg('xmax', type=float)
+    ymin = bot.get_arg('ymin', type=float)
+    ymax = bot.get_arg('ymax', type=float)
 
-    plt.savefig(filename_without_ext + '-line-plot.png')
+    plt.xlim([xmin, xmax])
+    plt.ylim([ymin, ymax])
+
+    outname = bot.no_extension() + '-line-plot.png'
+    outpath = bot.output_filepath(outname)
+
+    plt.savefig(outpath)
+
+if __name__ == "__main__":
+    main()
